677d7b5c039a79d107d26a17d7cc3a8e16fc3f44
MalenaSclippa <msclippa@mail.austral.edu.ar>
2025-04-11 12:38:15 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,3 +1,9 @@
-def slice_advanced():
-	texto=input("Ingrese un texto: ")
-	print(texto[4::2])
+def slice_simple():
+    texto = "Awesome"
+    primeras = texto[0:3].lower()
+    medio = texto[2:5].lower()
+    combinado = texto[0:4].lower() + texto[-3:].lower()
+texto[-3:].lower()
+    print(primeras)
+    print(medio)
+    print(combinado)
