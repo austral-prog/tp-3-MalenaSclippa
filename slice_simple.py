994f8c220cbd70e93bd0e9e68bd3bdf9f66bb56b
MalenaSclippa <msclippa@mail.austral.edu.ar>
2025-04-11 12:43:33 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -3,7 +3,6 @@ def slice_simple():
     primeras = texto[0:3].lower()
     medio = texto[2:5].lower()
     combinado = texto[0:4].lower() + texto[-3:].lower()
-texto[-3:].lower()
     print(primeras)
     print(medio)
     print(combinado)
